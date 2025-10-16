09ea383a0e28b28279280d41d71a813ffb144327
陈志和 <aaron.chen@QUECTEL.COM>
2025-10-16 16:55:24 +0800

    tracker box

---
+++ extensions/__init__.py
@@ -0,0 +1,11 @@
+from .qth_client import QthClient
+from .gnss_service import GnssService
+from .lbs_service import LbsService
+from .battery_service import BatteryService
+#from .sensor_service import SensorService
+
+qth_client = QthClient()
+gnss_service = GnssService()
+lbs_service = LbsService()
+battery_service = BatteryService()
+#sensor_service = SensorService()
